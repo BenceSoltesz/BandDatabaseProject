b58aa52344083a8448a95b08e6e2c8ede956b566
BenceSoltesz <solteszbence@stud.uni-obuda.hu>
2023-12-19 09:24:42 +0100

    Shared/Models/Manager added

---
+++ BandDatabaseProject.Client.Models/Manager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace BandDatabaseProject.Client.Models
+{
+    internal class Manager
+    {
+
+
+        public Manager()
+        {
+            ManagedBands = new HashSet<Band>();
+        }
+        public Manager(string input)
+        {
+            string[] split = input.Split('#');
+            ManagerId = int.Parse(split[0]);
+            ManagerName = split[1];
+        }
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ManagerId { get; set; }
+
+        public string ManagerName { get; set; }
+        [JsonIgnore]
+        public virtual ICollection<Band> ManagedBands { get; set; }
+    }
+}
