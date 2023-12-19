22618faf6b74f8863288663269d3bf550e2765c8
BenceSoltesz <solteszbence@stud.uni-obuda.hu>
2023-12-19 09:22:44 +0100

    /Shared/Models/Band added

--- BandDatabaseProject.Client.Models/Band.cs
+++ BandDatabaseProject.Client.Models/Band.cs
@@ -1,7 +1,55 @@
-﻿namespace BandDatabaseProject.Client.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+using System;
+
+namespace BandDatabaseProject.Client.Models
 {
-    public class Class1
+    public class Band
     {
+
+        public Band()
+        {
+            Concerts = new HashSet<Concert>();
+            LongPlayings = new HashSet<LongPlaying>();
+        }
+        public Band(string input)
+        {
+            string[] split = input.Split('#');
+            BandId = int.Parse(split[0]);
+            BandName = split[1];
+            Genre = split[2];
+            Origin = split[3];
+            ManagerId = int.Parse(split[4]);
+            Established = DateTime.Parse(split[5].Replace('-', '.'));
+
+        }
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int BandId { get; set; }
+        [Required]
+        public string BandName { get; set; }
+        public string Genre { get; set; }
+        public string Origin { get; set; }
+        public int ManagerId { get; set; }
+
+        public DateTime Established { get; set; }
+        [JsonIgnore]
+        public virtual Manager Manager { get; set; }
+        [JsonIgnore]
+        public virtual ICollection<Concert> Concerts { get; set; }
+        [JsonIgnore]
+        public virtual ICollection<LongPlaying> LongPlayings { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            var o = obj as Band;
+            return o != null && o.BandName.Equals(this.BandName);
+        }
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(this.BandName);
+        }
 
     }
 }
